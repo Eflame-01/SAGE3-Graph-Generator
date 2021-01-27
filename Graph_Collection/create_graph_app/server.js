ea2f0fe57eaac5a8564c73c1914b2636746c1903
Ese <eomene2@uic.edu>
2021-01-27 13:42:59 -0600

    Created way to upload information from the browser to the server

--- Graph_Collection/create_graph_app/server.js
+++ Graph_Collection/create_graph_app/server.js
@@ -5,6 +5,9 @@ var express = require('express');
 var app = express();
 const http = require('http').Server(app);
 
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 //init variables needed for writing files onto the page
 var fs = require('fs');
 
@@ -29,5 +32,26 @@ pythonProcess.stdout.on('data', (data) => {
   mystr = data.toString();
   myjson = JSON.parse(mystr)
 
-  console.log(myjson.Data)
+  //console.log(myjson.Data)
 })
+
+var writeFile = function(file) {
+  // write file
+  fs.writeFile("./data/data_information.txt", file, function(err){
+    if(err){
+      console.log(err.message);
+    }
+    else{
+      console.log("this is when we write the data and send it to our files.");
+    }
+  })
+};
+
+app.post('/', urlencodedParser, function(req, res) {
+  console.log(req.body);
+  res.sendStatus(200);
+  var file = req.body.file;
+
+  //if we get here, then we can create the file
+  writeFile(file);
+});
